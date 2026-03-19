c31a6e5ab9be4134edd8b95b6cf41213cbf88afe
philipz <philipzheng@gmail.com>
2026-03-19 19:39:28 +0800

    exp: XGB gap reg n=3500 lr=0.008

--- train.py
+++ train.py
@@ -254,9 +254,9 @@ USE_XGB_CLF = True
 
 # Gap Value Regressor (XGBRegressor)
 GAP_REG_PARAMS = dict(
-    n_estimators=3000,
+    n_estimators=3500,
     max_depth=4,
-    learning_rate=0.01,
+    learning_rate=0.008,
     subsample=0.75,
     colsample_bytree=0.7,
     reg_alpha=0.1,
